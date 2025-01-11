f502ddbf61ca39c4ee04766607a1f1f4cfc45c50
Treeshold <treeshold@treesholdapi.ml>
2025-01-11 17:58:48 +0300

    Initial commit

---
+++ RBWR Calculator/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace RBWR_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.extraNote = new System.Windows.Forms.Label();
+            this.outputFWFlow = new System.Windows.Forms.TextBox();
+            this.labelFWFlow = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPlantUsage = new System.Windows.Forms.Label();
+            this.labelDemand = new System.Windows.Forms.Label();
+            this.warningPrecision = new System.Windows.Forms.Label();
+            this.inputPlantUsage = new System.Windows.Forms.TextBox();
+            this.outputAPR = new System.Windows.Forms.TextBox();
+            this.inputMWe = new System.Windows.Forms.TextBox();
+            this.groupColdRun = new System.Windows.Forms.GroupBox();
+            this.labelCRExtraText = new System.Windows.Forms.Label();
+            this.labelCRRepairsTime = new System.Windows.Forms.Label();
+            this.inputCRTimeRepairs = new System.Windows.Forms.TextBox();
+            this.inputCRSoloPumpOut = new System.Windows.Forms.TextBox();
+            this.inputCRRVPOutflow = new System.Windows.Forms.TextBox();
+            this.labelCRPumpCapacity = new System.Windows.Forms.Label();
+            this.labelCROutflow = new System.Windows.Forms.Label();
+            this.labelPumpDeficit = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupColdRun.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.extraNote);
+            this.groupBox1.Controls.Add(this.outputFWFlow);
+            this.groupBox1.Controls.Add(this.labelFWFlow);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.labelPlantUsage);
+            this.groupBox1.Controls.Add(this.labelDemand);
+            this.groupBox1.Controls.Add(this.warningPrecision);
+            this.groupBox1.Controls.Add(this.inputPlantUsage);
+            this.groupBox1.Controls.Add(this.outputAPR);
+            this.groupBox1.Controls.Add(this.inputMWe);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 237);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MWe to APR";
+            // 
+            // extraNote
+            // 
+            this.extraNote.Location = new System.Drawing.Point(6, 187);
+            this.extraNote.Name = "extraNote";
+            this.extraNote.Size = new System.Drawing.Size(239, 47);
+            this.extraNote.TabIndex = 9;
+            this.extraNote.Text = "No calculations performed.";
+            this.extraNote.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // outputFWFlow
+            // 
+            this.outputFWFlow.Location = new System.Drawing.Point(112, 161);
+            this.outputFWFlow.Name = "outputFWFlow";
+            this.outputFWFlow.ReadOnly = true;
+            this.outputFWFlow.Size = new System.Drawing.Size(133, 20);
+            this.outputFWFlow.TabIndex = 8;
+            // 
+            // labelFWFlow
+            // 
+            this.labelFWFlow.Location = new System.Drawing.Point(6, 164);
+            this.labelFWFlow.Name = "labelFWFlow";
+            this.labelFWFlow.Size = new System.Drawing.Size(100, 23);
+            this.labelFWFlow.TabIndex = 7;
+            this.labelFWFlow.Text = "Feedwater Flow";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "APR (approx)";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelPlantUsage
+            // 
+            this.labelPlantUsage.Location = new System.Drawing.Point(6, 100);
+            this.labelPlantUsage.Name = "labelPlantUsage";
+            this.labelPlantUsage.Size = new System.Drawing.Size(100, 23);
+            this.labelPlantUsage.TabIndex = 5;
+            this.labelPlantUsage.Text = "Plant usage (MWe)";
+            this.labelPlantUsage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelDemand
+            // 
+            this.labelDemand.Location = new System.Drawing.Point(6, 77);
+            this.labelDemand.Name = "labelDemand";
+            this.labelDemand.Size = new System.Drawing.Size(100, 23);
+            this.labelDemand.TabIndex = 4;
+            this.labelDemand.Text = "Demand (MWe)";
+            this.labelDemand.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // warningPrecision
+            // 
+            this.warningPrecision.Location = new System.Drawing.Point(6, 16);
+            this.warningPrecision.Name = "warningPrecision";
+            this.warningPrecision.Size = new System.Drawing.Size(239, 58);
+            this.warningPrecision.TabIndex = 3;
+            this.warningPrecision.Text = resources.GetString("warningPrecision.Text");
+            // 
+            // inputPlantUsage
+            // 
+            this.inputPlantUsage.Location = new System.Drawing.Point(112, 103);
+            this.inputPlantUsage.Name = "inputPlantUsage";
+            this.inputPlantUsage.Size = new System.Drawing.Size(133, 20);
+            this.inputPlantUsage.TabIndex = 2;
+            this.inputPlantUsage.Text = "61.32";
+            this.inputPlantUsage.TextChanged += new System.EventHandler(this.ForceRecalculationLoad);
+            // 
+            // outputAPR
+            // 
+            this.outputAPR.Location = new System.Drawing.Point(112, 138);
+            this.outputAPR.Name = "outputAPR";
+            this.outputAPR.ReadOnly = true;
+            this.outputAPR.Size = new System.Drawing.Size(133, 20);
+            this.outputAPR.TabIndex = 1;
+            // 
+            // inputMWe
+            // 
+            this.inputMWe.Location = new System.Drawing.Point(112, 77);
+            this.inputMWe.Name = "inputMWe";
+            this.inputMWe.Size = new System.Drawing.Size(133, 20);
+            this.inputMWe.TabIndex = 0;
+            this.inputMWe.TextChanged += new System.EventHandler(this.ForceRecalculationLoad);
+            // 
+            // groupColdRun
+            // 
+            this.groupColdRun.Controls.Add(this.labelCRExtraText);
+            this.groupColdRun.Controls.Add(this.labelCRRepairsTime);
+            this.groupColdRun.Controls.Add(this.inputCRTimeRepairs);
+            this.groupColdRun.Controls.Add(this.inputCRSoloPumpOut);
+            this.groupColdRun.Controls.Add(this.inputCRRVPOutflow);
+            this.groupColdRun.Controls.Add(this.labelCRPumpCapacity);
+            this.groupColdRun.Controls.Add(this.labelCROutflow);
+            this.groupColdRun.Controls.Add(this.labelPumpDeficit);
+            this.groupColdRun.Location = new System.Drawing.Point(278, 12);
+            this.groupColdRun.Name = "groupColdRun";
+            this.groupColdRun.Size = new System.Drawing.Size(275, 237);
+            this.groupColdRun.TabIndex = 1;
+            this.groupColdRun.TabStop = false;
+            this.groupColdRun.Text = "Pump Deficit Run";
+            // 
+            // labelCRExtraText
+            // 
+            this.labelCRExtraText.Location = new System.Drawing.Point(6, 187);
+            this.labelCRExtraText.Name = "labelCRExtraText";
+            this.labelCRExtraText.Size = new System.Drawing.Size(263, 47);
+            this.labelCRExtraText.TabIndex = 7;
+            this.labelCRExtraText.Text = "No calculations performed";
+            this.labelCRExtraText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCRRepairsTime
+            // 
+            this.labelCRRepairsTime.Location = new System.Drawing.Point(6, 150);
+            this.labelCRRepairsTime.Name = "labelCRRepairsTime";
+            this.labelCRRepairsTime.Size = new System.Drawing.Size(148, 23);
+            this.labelCRRepairsTime.TabIndex = 6;
+            this.labelCRRepairsTime.Text = "Expected Repairs Time";
+            this.labelCRRepairsTime.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // inputCRTimeRepairs
+            // 
+            this.inputCRTimeRepairs.Location = new System.Drawing.Point(169, 152);
+            this.inputCRTimeRepairs.Name = "inputCRTimeRepairs";
+            this.inputCRTimeRepairs.Size = new System.Drawing.Size(100, 20);
+            this.inputCRTimeRepairs.TabIndex = 5;
+            this.inputCRTimeRepairs.Text = "330";
+            this.inputCRTimeRepairs.TextChanged += new System.EventHandler(this.ForceRecalculationColdRun);
+            // 
+            // inputCRSoloPumpOut
+            // 
+            this.inputCRSoloPumpOut.Location = new System.Drawing.Point(169, 126);
+            this.inputCRSoloPumpOut.Name = "inputCRSoloPumpOut";
+            this.inputCRSoloPumpOut.Size = new System.Drawing.Size(100, 20);
+            this.inputCRSoloPumpOut.TabIndex = 4;
+            this.inputCRSoloPumpOut.TextChanged += new System.EventHandler(this.ForceRecalculationColdRun);
+            // 
+            // inputCRRVPOutflow
+            // 
+            this.inputCRRVPOutflow.Location = new System.Drawing.Point(169, 100);
+            this.inputCRRVPOutflow.Name = "inputCRRVPOutflow";
+            this.inputCRRVPOutflow.Size = new System.Drawing.Size(100, 20);
+            this.inputCRRVPOutflow.TabIndex = 3;
+            this.inputCRRVPOutflow.TextChanged += new System.EventHandler(this.ForceRecalculationColdRun);
+            // 
+            // labelCRPumpCapacity
+            // 
+            this.labelCRPumpCapacity.Location = new System.Drawing.Point(6, 124);
+            this.labelCRPumpCapacity.Name = "labelCRPumpCapacity";
+            this.labelCRPumpCapacity.Size = new System.Drawing.Size(111, 22);
+            this.labelCRPumpCapacity.TabIndex = 2;
+            this.labelCRPumpCapacity.Text = "Solo Pump Capacity";
+            this.labelCRPumpCapacity.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelCROutflow
+            // 
+            this.labelCROutflow.Location = new System.Drawing.Point(6, 98);
+            this.labelCROutflow.Name = "labelCROutflow";
+            this.labelCROutflow.Size = new System.Drawing.Size(111, 23);
+            this.labelCROutflow.TabIndex = 1;
+            this.labelCROutflow.Text = "RPV Outflow ";
+            this.labelCROutflow.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // labelPumpDeficit
+            // 
+            this.labelPumpDeficit.Location = new System.Drawing.Point(6, 15);
+            this.labelPumpDeficit.Name = "labelPumpDeficit";
+            this.labelPumpDeficit.Size = new System.Drawing.Size(263, 71);
+            this.labelPumpDeficit.TabIndex = 0;
+            this.labelPumpDeficit.Text = resources.GetString("labelPumpDeficit.Text");
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(565, 261);
+            this.Controls.Add(this.groupColdRun);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "RBWR thingy";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupColdRun.ResumeLayout(false);
+            this.groupColdRun.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.TextBox inputCRTimeRepairs;
+        private System.Windows.Forms.Label labelCRRepairsTime;
+
+        private System.Windows.Forms.Label labelCROutflow;
+
+        private System.Windows.Forms.TextBox inputCRSoloPumpOut;
+
+        private System.Windows.Forms.TextBox inputCRRVPOutflow;
+
+        private System.Windows.Forms.Label labelCRExtraText;
+
+        private System.Windows.Forms.Label labelPumpDeficit;
+
+        private System.Windows.Forms.GroupBox groupColdRun;
+
+        private System.Windows.Forms.Label extraNote;
+
+        private System.Windows.Forms.TextBox outputFWFlow;
+
+        private System.Windows.Forms.Label labelFWFlow;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label labelDemand;
+
+        private System.Windows.Forms.Label warningPrecision;
+        private System.Windows.Forms.Label labelPlantUsage;
+
+        private System.Windows.Forms.TextBox inputPlantUsage;
+
+        private System.Windows.Forms.TextBox inputMWe;
+        private System.Windows.Forms.TextBox outputAPR;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+
+        private System.Windows.Forms.Label labelCRPumpCapacity;
+
+        #endregion
+    }
+}
---
+++ RBWR Calculator/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+// ReSharper disable LocalizableElement
+
+namespace RBWR_Calculator
+{
+    // Fun fact: this code sucks, but it works. It's a calculator for a fictional nuclear reactor anyway which I made because I was bored soloing unit 2.
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ForceRecalculationLoad(object sender, EventArgs e)
+        {
+            outputAPR.BackColor = Color.Red;
+            outputFWFlow.BackColor = Color.Red;
+
+            bool fla = !double.TryParse(inputMWe.Text, out var mwResult);
+            bool flb = !double.TryParse(inputPlantUsage.Text, out var plantUsageResult);
+            if (fla || flb)
+            {
+                this.outputAPR.Text = "Error: NaN";
+                this.outputFWFlow.Text = "Error: NaN";
+                extraNote.Text = "At least one value is not a number!";
+                return;
+            }
+
+            if (mwResult < 0 || plantUsageResult < 0)
+            {
+                this.outputAPR.Text = "Error: Negative";
+                this.outputFWFlow.Text = "Error: Negative";
+                extraNote.Text = "At least one value supplied is negative!";
+                return;
+            }
+
+            double totalRequested = mwResult + plantUsageResult;
+
+            double apr = (totalRequested + 163) / 14.3;
+            double flow = 82.8 + (13.7 * apr) + (5.87 * Math.Pow(10, -3) * Math.Pow(apr, 2));
+
+            outputAPR.BackColor = Color.GhostWhite;
+            outputFWFlow.BackColor = Color.GhostWhite;
+            List<string> notes = new List<string>();
+            if (flow > 1212.5)
+            {
+                outputFWFlow.BackColor = Color.DarkOrange;
+                notes.Add("Pump repairs may be impossible.");
+            }
+            else if (flow > 1187.5)
+            {
+                outputFWFlow.BackColor = Color.Orange;
+                notes.Add("Pump repairs may be unsafe, reactor level will drop below -3m.");
+            }
+            else if (flow > 990)
+            {
+                outputFWFlow.BackColor = Color.Yellow;
+                notes.Add("Pump repairs may possible, use 'Pump Deficit Run' to check.");
+            }
+
+            string extraTextLoad = "";
+            if (apr > 108)
+            {
+                extraTextLoad = "Danger, APR > 108%!";
+                outputAPR.BackColor = Color.Red;
+            }
+            else if (apr > 100)
+            {
+                extraTextLoad = "Warning, high load.";
+                outputAPR.BackColor = Color.DarkOrange;
+            }
+
+            if (extraTextLoad != "")
+            {
+                notes.Add(extraTextLoad);
+            }
+
+            extraNote.Text = "";
+            for (int i = 0; i < notes.Count; i++)
+            {
+                if (i < notes.Count)
+                {
+                    // ReSharper disable once LocalizableElement
+                    extraNote.Text += "\n";
+                }
+
+                extraNote.Text += notes[i];
+            }
+
+            if (extraNote.Text == "")
+            {
+                extraNote.Text = "All clear!";
+            }
+
+            this.outputAPR.Text = $@"{apr:0.00}";
+            this.outputFWFlow.Text = $@"{flow:0.00}";
+            this.inputCRRVPOutflow.Text = $@"{flow:0.00}";
+        }
+
+        private void ForceRecalculationColdRun(object sender = null, EventArgs e = null)
+        {
+            bool fla = !double.TryParse(inputCRRVPOutflow.Text, out var coreOutflow);
+            bool flb = !double.TryParse(inputCRSoloPumpOut.Text, out var pumpOutflow);
+            bool flc = !double.TryParse(inputCRTimeRepairs.Text, out var repairTime);
+
+            if (fla || flb || flc)
+            {
+                labelCRExtraText.Text = "At least one value is not a number!";
+                return;
+            }
+
+            if (pumpOutflow < 0 || coreOutflow < 0 || repairTime < 0)
+            {
+                // to add, at least one value is negative
+                labelCRExtraText.Text = "At least one value is negative!";
+                return;
+            }
+
+            if (pumpOutflow > 1000)
+            {
+                // one pump is able to output 1000 kg/s max
+                labelCRExtraText.Text = "Solo pump output is too high! 1000 kg/s max!";
+                return;
+            }
+
+            double reactorDeficit = coreOutflow - pumpOutflow;
+            if (reactorDeficit <= 0)
+            {
+                // no deficit
+                labelCRExtraText.Text = "No deficit, no repairs needed!";
+                return;
+            }
+
+            // we don't care about condenser or deaerator capacity per meter, since only core output matters.
+            // Condenser can be made up from CST, and deaerator should have the same output as hotwell.
+            // Solo pump output should be capped at the lowest of condenser pumps or feedwater pumps.
+            const double coreCapacityPerMeter = 7500;
+
+            // More than 5 meters will cause RPV drain, less than -4 meters will scram the reactor. Since MCC is hard, we'll just use 4.5m as the initial level.
+            const double coreInitialLevel = 4.5;
+
+            double totalWaterDeficit = reactorDeficit * repairTime;
+            double levelAtTheEnd = coreInitialLevel - (totalWaterDeficit / coreCapacityPerMeter);
+
+            string extraText;
+            if (levelAtTheEnd < -4)
+            {
+                extraText = "Reactor will scram, repairs impossible at this load";
+            }
+            else if (levelAtTheEnd < -3.8)
+            {
+                extraText = "Repairs possible, but not recommended, as the reactor will get quite thirsty";
+            }
+            else if (levelAtTheEnd < -3)
+            {
+                extraText = "Repairs possible, reactor will be thirsty though according to annunciation panel";
+            }
+            else
+            {
+                extraText = "Repairs possible!";
+            }
+
+            extraText += $" Reactor expected level: {levelAtTheEnd:0.00}m";
+            labelCRExtraText.Text = extraText;
+        }
+    }
+}
